25adad3c505f1573cc4c244aae6b1f2edc00bb2d
Thando Radebe <st10295345@rcconnect.edu.za>
2023-07-03 12:22:54 +0200

    Add project files.

---
+++ RecipeApp/RecipeForm.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RecipeApp
+{
+    public partial class RecipeForm : Form
+    {
+        private Dictionary<string, Recipe> recipes = new Dictionary<string, Recipe>();
+
+        public RecipeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRecipeButton_Click(object sender, EventArgs e)
+        {
+            // AddRecipe code here
+        }
+
+        private void DeleteRecipeButton_Click(object sender, EventArgs e)
+        {
+            // DeleteRecipe code here
+        }
+
+        private void ScaleRecipeButton_Click(object sender, EventArgs e)
+        {
+            // ScaleRecipe code here
+        }
+
+        private void DisplayRecipeButton_Click(object sender, EventArgs e)
+        {
+            // DisplayRecipe code here
+        }
+
+        private void FilterRecipesButton_Click(object sender, EventArgs e)
+        {
+            // FilterRecipes code here
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    // Recipe class (unchanged)
+    class Recipe
+    {
+        // Recipe class code here
+    }
+}
